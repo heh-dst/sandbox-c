c5093246aed1b7c5c225622366ac80b518174867
François Roland <francois.roland@heh.be>
2025-02-19 08:52:00 +0000

    Setup C++ toolchain.

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main(void) {
-  printf("Hello World!\n");
-  return 0;
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
 }
